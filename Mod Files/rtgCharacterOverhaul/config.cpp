558c775a4fa6760d291b1554a66725ae6251412a
ShrikeKmF <64348318+ShrikeKmF@users.noreply.github.com>
2025-09-01 16:19:49 +1200

    Ladder Spead Increase

--- Mod Files/rtgCharacterOverhaul/config.cpp
+++ Mod Files/rtgCharacterOverhaul/config.cpp
@@ -122,3 +122,20 @@ class RscDisplayMain: RscStandardDisplay {
         };
     };
 };
+
+class CfgMovesBasic;
+class CfgMovesMaleSdr: CfgMovesBasic
+{
+	class States
+	{
+		class LadderCivilStatic;
+		class LadderCivilUpLoop: LadderCivilStatic
+		{
+			speed = "1.05/(2/3)";
+		};
+	};
+};
+class CfgAnimation
+{
+	ladderSpeed = "0.5*2";
+};
