f2e61756b1c89b4b41313512d11542d4da0f34bd
ShrikeKmF <64348318+ShrikeKmF@users.noreply.github.com>
2025-10-07 18:05:59 +1300

    Fixed Incorect Class Names

--- Mod Files/rtgCustomContent/Config/Equipment/Helmets/CRTG Soft.hpp
+++ Mod Files/rtgCustomContent/Config/Equipment/Helmets/CRTG Soft.hpp
@@ -1,188 +1,188 @@
-class CTRG_cap_01_arid : ItemCore {
+class CTRG_cap_01_Arid : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "arid"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_arid : ItemCore {
+class CTRG_cap_01_goggles_Arid : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "arid"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_arid : ItemCore {
+class CTRG_cap_02_Arid : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "arid"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_arid : ItemCore {
+class CTRG_cap_02_goggles_Arid : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "arid"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_arid : ItemCore {
+class CTRG_cap_03_Arid : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "arid"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_arid : ItemCore {
+class CTRG_cap_03_goggles_Arid : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "arid"; variant = "3"; googles = "yes"; };
 };
 
-class CTRG_cap_01_des : ItemCore {
+class CTRG_cap_01_Desert : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "des"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_des : ItemCore {
+class CTRG_cap_01_goggles_Desert : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "des"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_des : ItemCore {
+class CTRG_cap_02_Desert : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "des"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_des : ItemCore {
+class CTRG_cap_02_goggles_Desert : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "des"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_des : ItemCore {
+class CTRG_cap_03_Desert : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "des"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_des : ItemCore {
+class CTRG_cap_03_goggles_Desert : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "des"; variant = "3"; googles = "yes"; };
 };
 
-class CTRG_cap_01_med : ItemCore {
+class CTRG_cap_01_Temperate : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "med"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_med : ItemCore {
+class CTRG_cap_01_goggles_Temperate : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "med"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_med : ItemCore {
+class CTRG_cap_02_Temperate : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "med"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_med : ItemCore {
+class CTRG_cap_02_goggles_Temperate : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "med"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_med : ItemCore {
+class CTRG_cap_03_Temperate : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "med"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_med : ItemCore {
+class CTRG_cap_03_goggles_Temperate : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "med"; variant = "3"; googles = "yes"; };
 };
 
-class CTRG_cap_01_nat : ItemCore {
+class CTRG_cap_01_Nature : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "nat"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_nat : ItemCore {
+class CTRG_cap_01_goggles_Nature : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "nat"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_nat : ItemCore {
+class CTRG_cap_02_Nature : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "nat"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_nat : ItemCore {
+class CTRG_cap_02_goggles_Nature : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "nat"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_nat : ItemCore {
+class CTRG_cap_03_Nature : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "nat"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_nat : ItemCore {
+class CTRG_cap_03_goggles_Nature : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "nat"; variant = "3"; googles = "yes"; };
 };
 
-class CTRG_cap_01_sha : ItemCore {
+class CTRG_cap_01_Shadow : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "sha"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_sha : ItemCore {
+class CTRG_cap_01_goggles_Shadow : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "sha"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_sha : ItemCore {
+class CTRG_cap_02_Shadow : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "sha"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_sha : ItemCore {
+class CTRG_cap_02_goggles_Shadow : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "sha"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_sha : ItemCore {
+class CTRG_cap_03_Shadow : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "sha"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_sha : ItemCore {
+class CTRG_cap_03_goggles_Shadow : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "sha"; variant = "3"; googles = "yes"; };
 };
 
-class CTRG_cap_01_temp : ItemCore {
+class CTRG_cap_01_Temp : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "temp"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_temp : ItemCore {
+class CTRG_cap_01_goggles_Temp : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "temp"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_temp : ItemCore {
+class CTRG_cap_02_Temp : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "temp"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_temp : ItemCore {
+class CTRG_cap_02_goggles_Temp : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "temp"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_temp : ItemCore {
+class CTRG_cap_03_Temp : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "temp"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_temp : ItemCore {
+class CTRG_cap_03_goggles_Temp : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "temp"; variant = "3"; googles = "yes"; };
 };
 
-class CTRG_cap_01_tun : ItemCore {
+class CTRG_cap_01_Tundra : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tun"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_tun : ItemCore {
+class CTRG_cap_01_goggles_Tundra : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tun"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_tun : ItemCore {
+class CTRG_cap_02_Tundra : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tun"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_tun : ItemCore {
+class CTRG_cap_02_goggles_Tundra : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tun"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_tun : ItemCore {
+class CTRG_cap_03_Tundra : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tun"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_tun : ItemCore {
+class CTRG_cap_03_goggles_Tundra : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tun"; variant = "3"; googles = "yes"; };
 };
 
-class CTRG_cap_01_tro : ItemCore {
+class CTRG_cap_01_Tropic : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tro"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_tro : ItemCore {
+class CTRG_cap_01_goggles_Tropic : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tro"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_tro : ItemCore {
+class CTRG_cap_02_Tropic : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tro"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_tro : ItemCore {
+class CTRG_cap_02_goggles_Tropic : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tro"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_tro : ItemCore {
+class CTRG_cap_03_Tropic : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tro"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_tro : ItemCore {
+class CTRG_cap_03_goggles_Tropic : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "tro"; variant = "3"; googles = "yes"; };
 };
 
-class CTRG_cap_01_urb : ItemCore {
+class CTRG_cap_01_Urban : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "urb"; variant = "1"; googles = "no"; };
 };
-class CTRG_cap_01_goggles_urb : ItemCore {
+class CTRG_cap_01_goggles_Urban : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "urb"; variant = "1"; googles = "yes"; };
 };
 
-class CTRG_cap_02_urb : ItemCore {
+class CTRG_cap_02_Urban : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "urb"; variant = "2"; googles = "no"; };
 };
-class CTRG_cap_02_goggles_urb : ItemCore {
+class CTRG_cap_02_goggles_Urban : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "urb"; variant = "2"; googles = "yes"; };
 };
 
-class CTRG_cap_03_urb : ItemCore {
+class CTRG_cap_03_Urban : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "urb"; variant = "3"; googles = "no"; };
 };
-class CTRG_cap_03_goggles_urb : ItemCore {
+class CTRG_cap_03_goggles_Urban : ItemCore {
     class XtdGearInfo { model = "ACEAX_Cap"; camo = "urb"; variant = "3"; googles = "yes"; };
 };
