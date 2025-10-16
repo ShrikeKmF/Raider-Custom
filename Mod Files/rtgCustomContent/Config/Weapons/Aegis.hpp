253e03b65894b177ef1067e0ad5ecb20c2932373
ShrikeKmF <64348318+ShrikeKmF@users.noreply.github.com>
2025-10-16 17:40:14 +1300

    Aegis Weapons Name Fix

--- Mod Files/rtgCustomContent/Config/Weapons/Aegis.hpp
+++ Mod Files/rtgCustomContent/Config/Weapons/Aegis.hpp
@@ -12,3 +12,177 @@ class arifle_ARX_base_F: Rifle_Base_F
         magazineWell[]            = { "50BW" };
     }
 }
+
+// SCARH
+class arifle_SCAR_base_F;
+class arifle_SCAR_F: arifle_SCAR_base_F
+{
+	displayName = "SCAR H";
+};
+class arifle_SCAR_GL_base_F;
+class arifle_SCAR_GL_F: arifle_SCAR_GL_base_F
+{
+	displayName = "SCAR H (GL)";
+};
+class arifle_SCAR_short_base_F;
+class arifle_SCAR_short_F: arifle_SCAR_short_base_F
+{
+	displayName = "SCAR H (CQB)";
+};
+class arifle_SCAR_grip_base_F;
+class arifle_SCAR_grip_F: arifle_SCAR_grip_base_F
+{
+	displayName = "SCAR H (Grip)";
+};
+class arifle_SCAR_black_base_F;
+class arifle_SCAR_black_F: arifle_SCAR_black_base_F
+{
+	displayName = "SCAR H (Black)";
+};
+class arifle_SCAR_GL_black_base_F;
+class arifle_SCAR_GL_black_F: arifle_SCAR_GL_black_base_F
+{
+	displayName = "SCAR H (Black/GL)";
+};
+class arifle_SCAR_short_black_base_F;
+class arifle_SCAR_short_black_F: arifle_SCAR_short_black_base_F
+{
+	displayName = "SCAR H (Black/CQB)";
+};
+class arifle_SCAR_grip_black_base_F;
+class arifle_SCAR_grip_black_F: arifle_SCAR_grip_black_base_F
+{
+	displayName = "SCAR H (Black/Grip)";
+};
+
+class arifle_SCAR_khaki_base_F;
+class arifle_SCAR_khaki_F: arifle_SCAR_khaki_base_F
+{
+	displayName = "SCAR H (Khaki)";
+};
+class arifle_SCAR_GL_khaki_base_F;
+class arifle_SCAR_GL_khaki_F: arifle_SCAR_GL_khaki_base_F
+{
+	displayName = "SCAR H (Khaki/GL)";
+};
+class arifle_SCAR_short_khaki_base_F;
+class arifle_SCAR_short_khaki_F: arifle_SCAR_short_khaki_base_F
+{
+	displayName = "SCAR H (Khaki/CQB)";
+};
+class arifle_SCAR_grip_khaki_base_F;
+class arifle_SCAR_grip_khaki_F: arifle_SCAR_grip_khaki_base_F
+{
+	displayName = "SCAR H (Khaki/Grip)";
+};
+
+// SR 25 (M110/M111)
+class Aegis_arifle_SR25_base_F;
+class Aegis_arifle_SR25_blk_F: Aegis_arifle_SR25_base_F
+{
+	displayName = "SR-25 (Black)";
+};
+class Aegis_arifle_SR25_khk_F: Aegis_arifle_SR25_base_F
+{
+	displayName = "SR-25 (Khaki)";
+};
+class Aegis_arifle_SR25_snd_F: Aegis_arifle_SR25_base_F
+{
+	displayName = "SR-25 (Sand)";
+};
+
+class Aegis_arifle_SR25_MR_base_F;
+class Aegis_arifle_SR25_MR_blk_F: Aegis_arifle_SR25_MR_base_F
+{
+	displayName = "SR-25 MR (Black)";
+};
+class Aegis_arifle_SR25_MR_khk_F: Aegis_arifle_SR25_MR_base_F
+{
+	displayName = "SR-25 MR (Khaki)";
+};
+class Aegis_arifle_SR25_MR_snd_F: Aegis_arifle_SR25_MR_base_F
+{
+	displayName = "SR-25 MR (Sand)";
+};
+
+// FN MAG
+class Aegis_MMG_FNMAG_Base;
+
+class Aegis_MMG_FNMAG_F: Aegis_MMG_FNMAG_Base
+{
+	displayName = "FN MAG";
+};
+
+class Aegis_MMG_FNMAG_240_F: Aegis_MMG_FNMAG_Base
+{
+	displayName = "FN MAG (TACTICAL)";
+};
+
+// FAMAS
+class atlas_arifle_FamasG4_Base;
+class atlas_arifle_famasG4_Grip_F: atlas_arifle_FamasG4_Base
+{
+	displayName = "FAMAS G4 (Grip)";
+};
+
+class Atlas_Arifle_Famas_M203_base;
+class Atlas_Arifle_famasG4_GL_F: Atlas_Arifle_Famas_M203_base
+{
+	displayName = "FAMAS G4 (GL)";
+};
+
+// FORT 652
+class arifle_TRG21_F;
+class arifle_FORT652_F: arifle_TRG21_F
+{
+	displayName = "FORT-652";
+};
+
+class arifle_TRG20_F;
+class arifle_FORT651_F: arifle_TRG20_F
+{
+	displayName = "FORT-651";
+};
+
+class arifle_TRG21_GL_F;
+class arifle_FORT652_GL_F: arifle_TRG21_GL_F
+{
+	displayName = "FORT-652 (GL)";
+};
+
+// G36
+class arifle_G36_base_F;
+class arifle_G36_F: arifle_G36_base_F
+{
+	displayName = "G36";
+};
+
+class arifle_G36_GL_base_F;
+class arifle_G36_GL_F: arifle_G36_GL_base_F
+{
+	displayName = "G36 (GL)";
+};
+
+class arifle_G36C_base_F;
+class arifle_G36C_F: arifle_G36C_base_F
+{
+	displayName = "G36C";
+};
+
+class arifle_G36_base_F;
+class arifle_G36_Sand_F: arifle_G36_base_F
+{
+	displayName = "G36 (Sand)";
+};
+
+class arifle_G36_GL_base_F;
+class arifle_G36_GL_Sand_F: arifle_G36_GL_base_F
+{
+	displayName = "G36 (Sand/GL)";
+};
+
+class arifle_G36C_base_F;
+class arifle_G36C_Sand_F: arifle_G36C_base_F
+{
+	displayName = "G36C (Sand)";
+};
