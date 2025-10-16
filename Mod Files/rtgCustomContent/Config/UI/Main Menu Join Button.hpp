90d406d98edf46d6e4b14bfca20533e7aea1c2c0
ShrikeKmF <64348318+ShrikeKmF@users.noreply.github.com>
2025-10-16 17:39:15 +1300

    Comments

--- Mod Files/rtgCustomContent/Config/UI/Main Menu Join Button.hpp
+++ Mod Files/rtgCustomContent/Config/UI/Main Menu Join Button.hpp
@@ -1,5 +1,7 @@
 ﻿class RscControlsGroupNoScrollbars;
 class RscStandardDisplay;
+
+
 class RscDisplayMain: RscStandardDisplay {
     class controls {
         class GroupSingleplayer: RscControlsGroupNoScrollbars {
@@ -14,14 +16,18 @@ class RscDisplayMain: RscStandardDisplay {
                     text = "RTG Main Server";
                     tooltip = "Connect to RTG Main Server";
                     y = "(3 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
+                    // Buttons are 1.5 tall, 3rd item in the drop down
                     onbuttonclick = "connectToServer ['144.6.36.191', 2302, '']";
+                    // IP, Port, Password
                 };
                 class JoinTrainingServer: ServerBrowser {
                     idc = -1;
                     text = "RTG Training Server";
                     tooltip = "Connect to RTG Training Server";
                     y = "(4 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
+                    // Buttons are 1.5 tall, 4th item in the drop down
                     onbuttonclick = "connectToServer ['144.6.26.217', 2302, '']";
+
                 };
             };
         };
