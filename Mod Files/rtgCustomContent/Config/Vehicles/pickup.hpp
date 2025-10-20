0ab948bc460aa3abda0ca9528232f6c614ff3817
ShrikeKmF <64348318+ShrikeKmF@users.noreply.github.com>
2025-10-20 22:29:23 +1300

    Attempt to add the animations define -didnt work

--- Mod Files/rtgCustomContent/Config/Vehicles/pickup.hpp
+++ Mod Files/rtgCustomContent/Config/Vehicles/pickup.hpp
@@ -12,10 +12,38 @@ class rtg_lpb: Arlit_TOYOTA_LC_HZJ79_M240
 	canAccessMineDetector = 1;
 	ace_cargo_space = 8;
 	RTG_Vehicle_Gear
+
+    animationList[] =
+	{
+	    "Rear_1", 0,
+        "Rear_2", 1,
+        "satcom_x", 1,
+        "small_antennas", 1,
+        "Commo", 1,
+        "antenna", 0,
+        "bumper", 0,
+        "Flag", 1,
+	    "Flag2", 1,
+	    "Flag3", 1
+    };
 }
 
 class rtg_lpb_g : rtg_lpb
 {
 	side = RESISTANCE;
 	faction = "Raider_Tactical_G_F";
+
+    animationList[] =
+    {
+        "Rear_1", 0,
+        "Rear_2", 1,
+        "satcom_x", 1,
+        "small_antennas", 1,
+        "Commo", 1,
+        "antenna", 0,
+        "bumper", 0,
+        "Flag", 1,
+        "Flag2", 1,
+        "Flag3", 1
+    };
 }
